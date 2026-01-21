cff2286ad3ef6b22e16f9475ddf53044cf563090
telole <asololearya@gmail.com>
2026-01-21 14:51:06 +0700

    main Init

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import(
-	"backend/configs"
-		
+import (
+	"log"
+
+	"backend/routes"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main() { 
-	configs.ConnectDB()
+func main() {
+	dsn := "host=localhost user=postgres password=root dbname=db_juan port=5050 sslmode=disable"
 
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+
+	e := echo.New()
+
+	routes.InitRoutes(e, db)
+
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
