0ca3418aca65e5aef43f970fd7ff9770cf3229e4
telole <asololearya@gmail.com>
2026-01-23 09:10:01 +0700

    [+]define ticketRoutes

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"backend/controllers/auth"
 	"backend/controllers/profile"
+	"backend/controllers/tickets"
 	"backend/res/middleware"
 
 	"github.com/labstack/echo/v4"
@@ -10,18 +11,30 @@ import (
 )
 
 func InitRoutes(e *echo.Echo, db *gorm.DB) {
+	//definisi Controller
 	authController := &auth.AuthController{DB: db}
+	profileController := &profile.ProfileController{DB: db}
+	ticketController := &tickets.TicketController{DB: db}
 
-	auth := e.Group("/auth")
+	//AuthController Routes
+	auth := e.Group("/auth") 
 	auth.POST("/register", authController.Register)
 	auth.POST("/login", authController.Login)
 
 	authProtected := e.Group("/auth", middleware.AuthMiddleware)
 	authProtected.POST("/logout", authController.Logout)
 
-	profileController := &profile.ProfileController{DB: db}
 
 	api := e.Group("/api", middleware.AuthMiddleware)
 	api.GET("/me", profileController.GetProfile)
+
+	ticket := api.Group("/tickets")
+	{
+		ticket.POST("", ticketController.CreateTicket)
+		ticket.GET("", ticketController.GetTickets)
+		ticket.GET("/:id", ticketController.GetTicketByID)
+		ticket.PUT("/:id/status", ticketController.UpdateTicketStatus)
+		// ticket.DELETE("/:id", ticketController.DeleteTicket)
+	}
 }
 	
